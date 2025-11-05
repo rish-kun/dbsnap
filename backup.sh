28bd87bb5092f359b3f3f2601326e642638b49c5
QuantumShinobi <63300310+rish-kun@users.noreply.github.com>
2025-11-06 03:53:50 +0530

    make basics

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 psql -U postgres
 docker exec Oasis_2025-postgres pg_dump -U postgres -Fc -f backup.dump postgres
 
-docker cp Oasis_2025-postgres:/backup.dump ./backups/backup_$(date +%Y%m%d_%H%M%S).dump
+docker cp Oasis_2025-postgres:/backup.dump ./backups/backup_$(date +%Y%m%d_%H%M%S).dump 
