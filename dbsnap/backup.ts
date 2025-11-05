932299dc03452d78fb624c2978bb45bbab1a3287
QuantumShinobi <63300310+rish-kun@users.noreply.github.com>
2025-11-06 04:03:40 +0530

    fix maybe

--- dbsnap/backup.ts
+++ dbsnap/backup.ts
@@ -11,7 +11,29 @@ export default async function takeBackup(dbname: String = "postgres") {
   const password = process.env.SCRIPT_PASSWORD || "your_password_here";
   await $`mkdir -p ./backups`;
   console.log(`Starting backup for database: ${dbname}`);
-  await $`printf '%s\n' ${password} | sudo -S -p '' docker exec ${container} pg_dump -U postgres -Fc -f ${tmpPath} postgres`;
+  const proc = Bun.spawn({
+    cmd: [
+      "sudo",
+      "-S",
+      "-p",
+      "",
+      "docker",
+      "exec",
+      container,
+      "pg_dump",
+      "-U",
+      "postgres",
+      "-Fc",
+      "-f",
+      tmpPath,
+      "postgres",
+    ],
+    stdin: new TextEncoder().encode(password + "\n"),
+    stdout: "pipe",
+    stderr: "pipe",
+  });
+  const code = await proc.exited;
+
   console.log(`Backup created inside container at: ${tmpPath}`);
   await $`printf '%s\n' ${password} | sudo -S -p '' docker cp ${container}:${tmpPath} ./backups/${backupFileName}`;
   console.log(`Backup taken: ./backups/${backupFileName}`);
